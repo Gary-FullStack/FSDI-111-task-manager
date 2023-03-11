5a525bc5bbda557474b287a3973722d1662fc2b2
Gary M <gary.fullstackdev@gmail.com>
2023-03-10 19:33:37 -0500

    assignment 3 files

---
+++ setup.sql
@@ -0,0 +1,49 @@
+CREATE TABLE status (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128)
+);  
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    summary VARCHAR(256),
+    description TEXT,
+    status_id INTEGER,
+    active BOOLEAN DEFAULT 1,
+    FOREIGN KEY (status_id) REFERENCES status(id)
+);
+
+
+INSERT INTO status ( name ) VALUES ("to do");
+INSERT INTO status ( name ) VALUES ("in progress");
+INSERT INTO status ( name ) VALUES ("done");
+
+
+INSERT INTO task (
+    summary,
+    description,
+    status_id
+) VALUES (
+    "Do some stuff",
+    " Put stuff over there",
+    1
+);
+
+INSERT INTO task (
+    summary,
+    description,
+    status_id
+) VALUES (
+    "Mow the lawn",
+    "Trim the hedges",
+    1
+);
+
+INSERT INTO task (
+    summary,
+    description,
+    status_id
+) VALUES (
+    "wash the car",
+    "change the oil",
+    1
+);
